2e88c43a9d9c29c0e268abdb9cf3931de7f1f69c
rafalohn <rafael@lohn.com.br>
2014-09-20 17:56:01 -0300

    exploring

--- exploratory.R
+++ exploratory.R
@@ -1,8 +1,23 @@
 library(caret)
+library(rattle)
+
 testing<-read.csv("pml-testing.csv")
 training<-read.csv("pml-training.csv")
-columnNAs<-colSums(is.na(training))
-cleanTraining <- training[,columnNAs< 19000]
-columnEmpty<-colSums(cleanTraining=="")+colSums(cleanTraining=="#DIV/0!")
-cleanTraining <- cleanTraining[,columnEmpty< 19000]
-cleanTraining <- cleanTraining[,!(names(cleanTraining) %in% 'cvtd_timestamp')]
+
+columnsToRemove<-c('X','user_name','raw_timestamp_part_1','raw_timestamp_part_2','cvtd_timestamp')
+cleanTraining <- training[,!(names(training) %in% columnsToRemove)]
+remove(training)
+remove(columnsToRemove)
+
+columnCountNA<-colSums(is.na(cleanTraining))
+cleanTraining <- cleanTraining[,columnCountNA< 10]
+remove(columnCountNA)
+
+columnCountEmpty<-colSums(cleanTraining=="")+colSums(cleanTraining=="#DIV/0!")
+cleanTraining <- cleanTraining[,columnCountEmpty< 10]
+remove(columnCountEmpty)
+
+
+modFit<-train(classe ~.,method='rpart',data=cleanTraining)
+
+fancyRpartPlot(modFit$finalModel)
